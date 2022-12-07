77a2fe0d6f88402fee24546f29c91e8f79b18bfc
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-07 14:54:20 +0400

    Add swagger middleware for API

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 
 import connectToMongo from "./config/mongo.js";
 import feedbackRouter from "./routes/Feedback-router.js";
+import swaggerMiddleware from "./middlewares/swagger-middleware.js";
 
 const app = Express();
 dotenv.config();
 connectToMongo();
 
 app.use(bodyParser.json());
+
 app.use("/api", feedbackRouter);
+app.use("/", ...swaggerMiddleware());
 
 app.listen(3000);
