dbc716400960c8eaa8e871c6252b2620495f5487
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-14 18:51:53 +0400

    create feedback add request function with validation

--- src/controllers/Feedback-controller.js
+++ src/controllers/Feedback-controller.js
@@ -1,6 +1,34 @@
 import Feedback from "../models/Feedback.js";
+import addFeedbackSchema from "../schemas/add-feedback-schema.js";
 
 export const getAllFeedbacks = async (req, res) => {
   const data = await Feedback.find();
   return res.json(data);
+};
+
+export const addFeedback = async (req, res) => {
+  const { body } = req;
+  const validator = await addFeedbackSchema(body);
+  const { value, error } = validator.validate(body);
+
+  if (error) {
+    return res.status(401).json(error.details);
+  }
+
+  const { title, description, category_id } = value;
+
+  const lastFeedback = await Feedback.find().sort({ _id: -1 }).limit(1);
+
+  const id = lastFeedback.length > 0 ? lastFeedback[0].id + 1 : 1;
+
+  await Feedback.create({
+    title,
+    description,
+    category_id,
+    upvotes: 0,
+    status: "suggestion",
+    id: 1,
+  });
+
+  return res.status(201).json({ message: "Feedback created successfully" });
 };
