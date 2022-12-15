f53304c95bc7a248e4abc7cadea61d53a3e03162
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-15 19:37:53 +0400

    create comment model

--- src/models/Status.js
+++ src/models/Status.js
@@ -11,6 +11,10 @@ const statusSchema = new Schema({
     type: Schema.Types.Number,
     required: true,
   },
+  color: {
+    type: Schema.Types.String,
+    required: true,
+  },
 });
 
 const Status = mongoose.model("Status", statusSchema);
