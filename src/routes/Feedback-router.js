44593735e57e4b1659934ff61c6baa9a3ad29910
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-17 18:05:55 +0400

    create new router paths and request types in feedback router

--- src/routes/Feedback-router.js
+++ src/routes/Feedback-router.js
@@ -1,12 +1,18 @@
 import Express from "express";
 import {
   addFeedback,
+  deleteFeedback,
   getAllFeedbacks,
+  getSingleFeedback,
+  updateFeedback,
 } from "../controllers/Feedback-controller.js";
 
 const feedbackRouter = Express.Router();
 
-feedbackRouter.get("/feedbacks", getAllFeedbacks);
 feedbackRouter.post("/feedbacks/new", addFeedback);
+feedbackRouter.delete("/feedbacks/:id", deleteFeedback);
+feedbackRouter.get("/feedbacks", getAllFeedbacks);
+feedbackRouter.get("/feedbacks/:id", getSingleFeedback);
+feedbackRouter.put("/feedback", updateFeedback);
 
 export default feedbackRouter;
